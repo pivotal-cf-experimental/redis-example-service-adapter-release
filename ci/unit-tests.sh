1f042ea2d7b783a6ab5b7fe3635eee9fc5a84d17
Simon Jones <sijones@pivotal.io>
2017-11-21 09:56:42 +0000

    Get ginkgo directly
    [#151549249]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- ci/unit-tests.sh
+++ ci/unit-tests.sh
@@ -5,10 +5,7 @@ pushd $(dirname $0)/..
   export PATH=$GOPATH/bin:$PATH
 
   pushd src/github.com/pivotal-cf-experimental/redis-example-service-adapter
-    go get -v github.com/tools/godep
-    godep restore
-
-    go install -v github.com/onsi/ginkgo/ginkgo
+    go get -v github.com/onsi/ginkgo/ginkgo
 
     ./scripts/run-tests.sh
   popd
